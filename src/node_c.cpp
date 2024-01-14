2eb22458687527aa319031534bd81fcf8d87860c
Romina Contreras Zerega <146995126+RominaZe@users.noreply.github.com>
2024-01-14 15:52:41 +0100

    Update node_c.cpp change everything

--- src/node_c.cpp
+++ src/node_c.cpp
@@ -1,40 +1,68 @@
 #include "ros/ros.h"
-#include "assignment_2_2023/AverageSpeed.h"
-#include "geometry_msgs/Twist.h"
+#include "assignment_2_2023/RobotPV.h"
+#include "assignment_2_2023/Average.h"
+#include "assignment_2_2023/Distance.h"
+#include "nav_msgs/Odometry.h"
+#include "numeric"
 
-assignment_2_2023::AverageSpeed average_speed;
-int number_of_messages = 0;
-double sum_x = 0, sum_z = 0;
 
+double distance = 0, avg_speed_x = 0, avg_speed_z;
+std::vector<double> window_x, window_z;
 
-void SpeedCallback(const assignment_2_2023::AverageSpeed::ConstPtr& msg) {
-    sum_x += msg->linear_x;
-    sum_z += msg->angular_z;
-    number_of_messages++;
+void AverageCallback(const assignment_2_2023::RobotPV::ConstPtr& msg)
+{
+    double position_x, position_y;   
+    position_x = msg->x;
+    position_y = msg->y;
+    
+    double x_goal,y_goal;
+    ros::param::get("/des_pos_x",x_goal);
+    ros::param::get("/des_pos_y",y_goal);
+    
+    //Compute distance
+    distance = std::sqrt(std::pow(position_x-x_goal,2) + std::pow(position_y-y_goal,2));
+    
+    //Compute the average speed
+    int window_size;
+    ros::param::get("/window_size", window_size);
+    
+    if (window_x.size() > static_cast<size_t>(window_size)){
+        window_x.erase(window_x.begin()); }
+
+    if (window_z.size() > static_cast<size_t>(window_size)){
+        window_z.erase(window_z.begin());}
+
+    window_x.push_back(msg->linear_x);
+    window_z.push_back(msg->angular_z); 
+
+    avg_speed_x = std::accumulate(window_x.begin(), window_x.end(), 0.0) / window_size;
+    avg_speed_z = std::accumulate(window_z.begin(), window_z.end(), 0.0) / window_size;
 }
 
-bool AverageSpeedCallback(assignment_2_2023::AverageSpeed::Request& req,
-    assignment_2_2023::AverageSpeed::Response& res) {
-    if (number_of_messages == 0) {
-        ROS_INFO("No messages received");
-        return false;
-    }
-    else{
+bool DistSpeedCallback(assignment_2_2023::Average::Request& req,
+                 assignment_2_2023::Average::Response& res)
+{
+    res.linear_avg = avg_speed_x;
+    res.angular_avg = avg_speed_z;
+    res.distance = distance;
+    return true;
+}
 
-    res.average_speed.linear_x = sum_x / number_of_messages;
-    res.average_speed.angular_z = sum_z / number_of_messages;}
-    return true;}
-    
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "node_c_robot_pv");
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "node_c_part2");
     ros::NodeHandle nh;
+    
+    ros::ServiceServer service = nh.advertiseService("/distance_averageVelocity", DistSpeedCallback);
+    ros::Subscriber sub = nh.subscribe("/position_velocity", 1, AverageCallback);
+    ros::Rate loop_rate(10);
 
-    ros::Subscriber sub = nh.subscribe("/position_velocity", 1, SpeedCallback);
-    ros::ServiceServer service = nh.advertiseService("/average_speed", AverageSpeedCallback);
-
-    ros::spin();
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 
     return 0;
 }
-
